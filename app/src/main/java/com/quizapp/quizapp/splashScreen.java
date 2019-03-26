2f7d564f814a06dbf746e519e06aba321bc41da9
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-03-26 12:13:58 +0100

    fixed some bugs

--- app/src/main/java/com/quizapp/quizapp/splashScreen.java
+++ app/src/main/java/com/quizapp/quizapp/splashScreen.java
@@ -1,6 +1,7 @@
 package com.quizapp.quizapp;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class splashScreen extends AppCompatActivity {
@@ -9,5 +10,18 @@ public class splashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                
+            }
+        }
     }
+
+
+    )
+
 }

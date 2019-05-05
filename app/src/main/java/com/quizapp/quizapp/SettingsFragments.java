bec4ba76f32d3817111617a23a7e90b6db54ba98
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-05-05 16:19:56 +0100

    Modified the settings activity in the app

---
+++ app/src/main/java/com/quizapp/quizapp/SettingsFragments.java
@@ -0,0 +1,11 @@
+package com.quizapp.quizapp;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragments extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+
+    }
+}

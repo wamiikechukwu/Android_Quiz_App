10a2bbcf544c67a248bd4dd2e963545d60b17fc3
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-05-16 16:41:01 +0100

    resolved some bugs

--- app/src/main/java/com/quizapp/quizapp/didYouKnow.java
+++ app/src/main/java/com/quizapp/quizapp/didYouKnow.java
@@ -9,5 +9,6 @@ public class didYouKnow extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_did_you_know);
+
     }
 }

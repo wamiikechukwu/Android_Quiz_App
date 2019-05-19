ac6a35f85a5e34ed28d4789a74de1b202cc51b7c
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-05-19 16:08:09 +0100

    Make some improvements

--- app/src/main/java/com/quizapp/quizapp/didYouKnow.java
+++ app/src/main/java/com/quizapp/quizapp/didYouKnow.java
@@ -14,6 +14,7 @@ public class didYouKnow extends AppCompatActivity {
 
     }
 
+
     public void cardViewOne(View view) {
         Intent intent = new Intent(this, cardViewOne.class);
         startActivity(intent);

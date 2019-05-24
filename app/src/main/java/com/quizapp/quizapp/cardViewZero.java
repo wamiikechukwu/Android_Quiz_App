eda2eadc1b06c7c5880f49e6ff42d3752bde402a
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-05-24 17:15:28 +0100

    Applied some changes to the Did You Know layout

--- app/src/main/java/com/quizapp/quizapp/cardViewZero.java
+++ app/src/main/java/com/quizapp/quizapp/cardViewZero.java
@@ -5,21 +5,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class didYouKnow extends AppCompatActivity {
+public class cardViewZero extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_did_you_know);
+        setContentView(R.layout.activity_card_view_zero);
     }
 
-
-    public void cardViewZero(View view) {
-        Intent intent = new Intent(this, cardViewZero.class);
+    public void goBack(View view) {
+        Intent intent = new Intent(this, overView.class);
         startActivity(intent);
-    }
-
-    public void cardViewOne(View view) {
-
     }
 }

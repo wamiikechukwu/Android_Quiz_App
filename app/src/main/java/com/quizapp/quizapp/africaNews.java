48f90756cfa7ade357e978cfd681f75735520af5
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-05-25 22:11:21 +0100

    added codes for the web view

--- app/src/main/java/com/quizapp/quizapp/africaNews.java
+++ app/src/main/java/com/quizapp/quizapp/africaNews.java
@@ -2,12 +2,17 @@ package com.quizapp.quizapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 public class africaNews extends AppCompatActivity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_africa_news);
+
+        webView.loadUrl("https://www.africanews.com");
     }
 }

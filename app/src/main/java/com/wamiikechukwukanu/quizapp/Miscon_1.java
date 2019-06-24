a3f15f400ce0fb795a46de42abeedbfeb3381885
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-06-24 22:38:49 +0100

    Removed the fragment and decided to use an activity instead

--- app/src/main/java/com/wamiikechukwukanu/quizapp/Miscon_1.java
+++ app/src/main/java/com/wamiikechukwukanu/quizapp/Miscon_1.java
@@ -2,17 +2,12 @@ package com.wamiikechukwukanu.quizapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
-public class misconAfrica extends AppCompatActivity {
+public class Miscon_1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_miscon_africa);
-    }
-
-
-    public void misconceptionsOfAfrica(View view) {
+        setContentView(R.layout.activity_miscon_1);
     }
 }

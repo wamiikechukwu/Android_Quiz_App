ff7b730a21544115f5ebecc45571625b3434d4cd
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-06-24 23:39:39 +0100

    Adding the Misconception activities in the app

--- app/src/main/java/com/wamiikechukwukanu/quizapp/Miscon_2.java
+++ app/src/main/java/com/wamiikechukwukanu/quizapp/Miscon_2.java
@@ -1,16 +1,16 @@
 package com.wamiikechukwukanu.quizapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class Miscon_1 extends AppCompatActivity {
+public class Miscon_2 extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_miscon_1);
+        setContentView(R.layout.activity_miscon_2);
     }
 
     public void goBack(View view) {
@@ -18,8 +18,7 @@ public class Miscon_1 extends AppCompatActivity {
         startActivity(intent);
     }
 
+
     public void goNext(View view) {
-        Intent intent = new Intent(this, Miscon_2.class);
-        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/wamiikechukwukanu/quizapp/Miscon_3.java
@@ -0,0 +1,13 @@
+package com.wamiikechukwukanu.quizapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Miscon_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_miscon_3);
+    }
+}

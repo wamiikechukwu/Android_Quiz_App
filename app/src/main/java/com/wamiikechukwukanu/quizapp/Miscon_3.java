86e5698f14544fedbb22e605fbc92344a9e79fd7
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-06-25 00:10:42 +0100

    Adding the Misconception activities in the app

--- app/src/main/java/com/wamiikechukwukanu/quizapp/Miscon_3.java
+++ app/src/main/java/com/wamiikechukwukanu/quizapp/Miscon_3.java
@@ -1,7 +1,9 @@
 package com.wamiikechukwukanu.quizapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Miscon_3 extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Miscon_3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_miscon_3);
+    }
+
+    public void goBack(View view) {
+        Intent intent = new Intent(this, misconAfrica.class);
+        startActivity(intent);
+    }
+
+    public void goNext(View view) {
+        Intent intent = new Intent(this, Miscon_4.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/wamiikechukwukanu/quizapp/Miscon_4.java
+++ app/src/main/java/com/wamiikechukwukanu/quizapp/Miscon_4.java
@@ -1,26 +1,24 @@
 package com.wamiikechukwukanu.quizapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class Miscon_2 extends Activity {
+public class Miscon_4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_miscon_2);
+        setContentView(R.layout.activity_miscon_4);
     }
+
 
     public void goBack(View view) {
         Intent intent = new Intent(this, misconAfrica.class);
         startActivity(intent);
     }
 
-
     public void goNext(View view) {
-        Intent intent = new Intent(this, Miscon_3.class);
-        startActivity(intent);
     }
 }

2abaddae7dee020d1609872e4b98aa9233ac005b
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-08-26 14:29:10 +0100

    Making a major changes to the Misconception Activity.

--- app/src/main/java/com/wamiikechukwukanu/quizapp/misconAfrica.java
+++ app/src/main/java/com/wamiikechukwukanu/quizapp/misconAfrica.java
@@ -4,23 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
 
 public class misconAfrica extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-
         setContentView(R.layout.activity_miscon_africa);
     }
 
-
     public void misconceptionsOfAfrica(View view) {
+
         Intent intent = new Intent(this, Miscon_1.class);
         startActivity(intent);
     }
+
 }

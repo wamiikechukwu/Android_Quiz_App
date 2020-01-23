7b1ade64742cde6e0821c46a16d05039a13fb163
Wami ikechukwu <wamiikechukwu@gmail.com>
2020-01-23 14:32:54 +0100

    About to add the Introduction Screen to the App (V1.0)

---
+++ app/src/main/java/com/wamiikechukwukanu/quizapp/Intro_Screen/IntroActivity.java
@@ -0,0 +1,13 @@
+package com.wamiikechukwukanu.quizapp.Intro_Screen;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

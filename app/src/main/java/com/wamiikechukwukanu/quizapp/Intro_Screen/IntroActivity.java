798a4dfd341b2968eaf2bd22877aa5c1457b9d40
Wami ikechukwu <wamiikechukwu@gmail.com>
2020-02-17 11:07:09 +0100

    The first Introduction Screen failed, i am building a new one from scratch

--- app/src/main/java/com/wamiikechukwukanu/quizapp/Intro_Screen/IntroActivity.java
+++ app/src/main/java/com/wamiikechukwukanu/quizapp/Intro_Screen/IntroActivity.java
@@ -2,6 +2,8 @@ package com.wamiikechukwukanu.quizapp.Intro_Screen;
 
 import android.os.Bundle;
 
+import com.wamiikechukwukanu.quizapp.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class IntroActivity extends AppCompatActivity {
@@ -9,5 +11,6 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
     }
 }

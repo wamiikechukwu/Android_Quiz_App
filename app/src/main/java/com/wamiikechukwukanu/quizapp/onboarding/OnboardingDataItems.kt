ef018d5169a9fe16ad9c512afe9c5fd1bcee09bb
wamiikechukwu <wamiikechukwu@gmail.com>
2020-07-13 09:04:27 +0100

    Added a data class that will hold all details of the onboarding Screen

---
+++ app/src/main/java/com/wamiikechukwukanu/quizapp/onboarding/OnboardingDataItems.kt
@@ -0,0 +1,7 @@
+package com.wamiikechukwukanu.quizapp.onboarding
+
+data class OnboardingDataItems(
+        var onboardingImage: Int,
+        var onboardingTitle: String,
+        var onboardingSubTitle: String
+)

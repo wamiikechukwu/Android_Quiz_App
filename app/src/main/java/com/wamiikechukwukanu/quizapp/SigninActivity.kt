1b539ba31744ab473ebe47271676761f032bced4
wamiikechukwu@gmail.com <Aleruchivera7470>
2021-02-03 11:21:54 +0100

    About to work on the Main Activity

--- app/src/main/java/com/wamiikechukwukanu/quizapp/SigninActivity.kt
+++ app/src/main/java/com/wamiikechukwukanu/quizapp/SigninActivity.kt
@@ -5,8 +5,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_signin.*
 
@@ -14,14 +21,25 @@ class SigninActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
+    lateinit var GoogleSignInClient: GoogleSignInClient
+
+    val RC_SIGN_IN = 123
+
+    // Access a Cloud Firestore instance from your Activity
+    val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
         supportActionBar?.title = ""
 
         auth = Firebase.auth
-    }
 
+        sign_in_button.setOnClickListener(View.OnClickListener {
+            createRequest()
+            signIn()
+        })
+    }
 
     fun validation() {
 //         GET THE CURRENT USER INPUTS
@@ -49,13 +67,18 @@ class SigninActivity : AppCompatActivity() {
                             progress_bar.visibility = View.INVISIBLE
                             // Sign in success, update UI with the signed-in user's information
                             val user = auth.currentUser
-                            Toast.makeText(baseContext, "Sign In Success",
+                            Toast.makeText(baseContext, "Sign In Successfully",
                                     Toast.LENGTH_LONG).show()
 
+                            //                            MOVE TO MAIN ACTIVITY
+                            intent = Intent(this, MainActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                            startActivity(intent)
+
                         } else {
-                            progress_bar.visibility = View.VISIBLE
+                            progress_bar.visibility = View.INVISIBLE
                             // If sign in fails, display a message to the user.
-                            Toast.makeText(baseContext, "Sign In Failed",
+                            Toast.makeText(baseContext, "Create an Account or Use Google Sign in",
                                     Toast.LENGTH_LONG).show()
                             // ...
                         }
@@ -64,6 +87,84 @@ class SigninActivity : AppCompatActivity() {
                     }
         }
     }
+
+    fun createRequest() {
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        GoogleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+
+    private fun signIn() {
+        val signInIntent = GoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+
+    fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = Firebase.auth.currentUser
+
+                        user?.let {
+
+                            // Add a new document with a generated ID
+                            val newUser = userRegistrationDetails(user.displayName.toString(), user.email.toString(), user.photoUrl.toString())
+
+                            db.collection("AfricanQuizAppUsers").document(user.email.toString())
+                                    .set(newUser)
+                                    .addOnSuccessListener {
+                                    }
+                                    .addOnFailureListener { e ->
+                                        Toast.makeText(this, "Google Details not Saved", Toast.LENGTH_LONG).show()
+                                    }
+
+                            // MOVE TO MAIN ACTIVITY
+                            Toast.makeText(this, "LOGIN", Toast.LENGTH_LONG).show()
+                            intent = Intent(this, MainActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                            startActivity(intent)
+
+//                            TODO
+                            //updateUI(user)
+                        }
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Snackbar.make(findViewById(R.id.signin), "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+//                            TODO
+                        // updateUI(null)
+                    }
+
+                    // ...
+                }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show()
+                // ...
+            }
+        }
+    }
+
 
     fun EmailLoginInButton(view: View) {
         validation()
@@ -78,5 +179,8 @@ class SigninActivity : AppCompatActivity() {
 
 
 }
+
+
+
 
 
--- app/src/main/java/com/wamiikechukwukanu/quizapp/SignupActivity.kt
+++ app/src/main/java/com/wamiikechukwukanu/quizapp/SignupActivity.kt
@@ -1,11 +1,13 @@
 package com.wamiikechukwukanu.quizapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_signup.*
 
@@ -13,12 +15,16 @@ class SignupActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
+    // Access a Cloud Firestore instance from your Activity
+    val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         supportActionBar?.hide()
 
         auth = Firebase.auth
+
     }
 
     fun signUpButton(view: View) {
@@ -71,11 +77,31 @@ class SignupActivity : AppCompatActivity() {
                             // Sign in success, update UI with the signed-in user's information
                             progress_bar.visibility = View.INVISIBLE
                             Toast.makeText(this, "Sign Up Success", Toast.LENGTH_LONG).show()
-                            val user = auth.currentUser
+
+                            val user = Firebase.auth.currentUser
+
+                            user?.let {
+                                // Add a new document with a generated ID
+                                val newUser = userRegistrationDetails(username_edit_text.text.toString(), user.email.toString(), user.photoUrl.toString())
+
+                                db.collection("AfricanQuizAppUsers").document(user.email.toString())
+                                        .set(newUser)
+                                        .addOnSuccessListener {
+                                        }
+                                        .addOnFailureListener { e ->
+                                            Toast.makeText(this, "Error adding document", Toast.LENGTH_LONG).show()
+                                        }
+//                            MOVE TO MAIN ACTIVITY
+                                intent = Intent(this, MainActivity::class.java)
+                                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                startActivity(intent)
+
+                            }
+
                         } else {
                             progress_bar.visibility = View.INVISIBLE
                             // If sign in fails, display a message to the user.
-                            Toast.makeText(baseContext, "Sign Up",
+                            Toast.makeText(baseContext, "Error while signing up",
                                     Toast.LENGTH_SHORT).show()
                         }
 
